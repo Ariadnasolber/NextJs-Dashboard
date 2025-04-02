e9622f173d19da7891a5ab8413c41bc5f49d8b55
Ariadnasolber <arisoler97@gmail.com>
2025-04-02 13:23:15 +0200

    Mutant data

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,24 @@
+//Componente de Servidor que obtiene customers y lo pasa al <Form> componente. 
+import Form from "@/app/ui/invoices/create-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers } from "@/app/lib/data";
+
+export default async function Page() {
+    const customers = await fetchCustomers();
+
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: "Invoices", href: "/dashboard/invoices" },
+                    {
+                        label: "Create Invoice",
+                        href: "/dashboard/invoices/create",
+                        active: true,
+                    },
+                ]}
+            />
+            <Form customers={customers} />
+        </main>
+    );
+}
